a930b45b16654488e5d2550a4a053b8092546c7a
jiyang <893257571@qq.com>
2019-08-12 15:24:40 +0800

    Commit changes into Git

---
+++ gitProject/src/com/heima/demo/Demo.java
@@ -0,0 +1,18 @@
+package com.heima.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hellow Git");
+        System.out.println("Hellow Git");
+        System.out.println("Hellow Git");
+        System.out.println("Hellow Git");
+        System.out.println("Hellow Git");
+        System.out.println("Hellow Git");
+    }
+
+    public void test1(){
+        int i=1;
+        i++;
+        System.out.println(i);
+    }
+}
